b3efdcc3dce1cb221616ff331eed79bf7dce0d7d
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2019-11-12 19:47:38 +0300

    Scala: Generic classes

---
+++ scalaTour/src/test/scala/scalaTour/0170-GenericClassesSpec.scala
@@ -0,0 +1,29 @@
+/**
+  * GENERIC CLASSES are classes which take a type as a parameter.
+  * They are particularly useful for collection classes.
+  */
+import org.scalatest.FlatSpec
+
+// Defining a generic class
+class Stack[A] {
+  private var elements: List[A] = Nil
+  def push(x: A) { elements = x :: elements }
+  def peek: A = elements.head
+  def pop(): A = {
+    val currentTop = peek
+    elements = elements.tail
+    currentTop
+  }
+}
+
+class GenericClassesExampleSpec extends FlatSpec {
+  behavior of "generic class"
+
+  it should "allow to use generic class" in {
+    val stack = new Stack[Int]
+    stack.push(1)
+    stack.push(2)
+    assert(stack.pop === 2)
+    assert(stack.pop === 1)
+  }
+}
